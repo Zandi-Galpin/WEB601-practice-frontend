c45e72b594f4eb000aefd6310738f85ad07a5d0f
Zandi Galpin <zandi-galpin@live.nmit.ac.nz>
2026-05-27 21:54:07 +1200

    added profile stuff and build thing

--- src/lib/server/profile.js
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from 'fs';
-
-const profile = JSON.parse(readFileSync('data/profile.json', 'utf8'));
-
-export function getProfile() {
-    return profile;
-}
--- src/routes/profile/+page.server.js
+++
@@ -1,8 +0,0 @@
-import * as store from '$lib/server/profile.js';
-
-export function load() {
-    return { profile: store.getProfile() };
-}
-
-
-
